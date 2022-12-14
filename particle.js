fefd6a7c089a308a169bf0c8b717bcac209f6689
jericodellomas <jericodellomas23@gmail.com>
2022-12-14 11:47:30 +0800

    update particle.js

--- particle.js
+++ particle.js
@@ -726,7 +726,7 @@ var pJS = function(tag_id, params){
     }
   
   
-    /* ---------- pJS functions - modes events ------------ */
+    
   
     pJS.fn.modes.pushParticles = function(nb, pos){
   
